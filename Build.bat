c4373ea02b619966567cc628f8fcc961d322383c
OldRazeJedy <bukovskiy14@gmail.com>
2022-10-23 22:42:47 +0300

    fix build bat

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 "G:\UE4\UE_4.27\Engine\Build\BatchFiles\RunUAT.bat" ^
 BuildCookRun ^
--project=F:\Practice\EducationAR\EducationAR.uproject ^
+-project=EducationAR.uproject ^
 -platform=Android -clientconfig=Development ^
 -cook -stage --pak -package -allmaps
